8da0c9ee5c2ee9cc9c2f6d7e84ea2906f293d54d
Surya Dutta <suryabrata.dutta@yale.edu>
2018-06-20 19:44:09 -0400

    more formatting

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ var mongo = require('../models/mongo')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   mongo.getData('entries',function(err,data){
+    data.sort(function(a,b){return new Date(b.updated) - new Date(a.updated);});
     res.render('index', { 
       title: 'Google Alerts MongoDB Test',
       data: data,
+      moment: require('moment')
     });
   })
 });
